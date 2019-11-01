e470257b6df463235084996b51d9ca0f736e39b5
Hanjun Kim <hallazzang@gmail.com>
2019-11-01 23:38:21 +0900

    render table to reflect the dataSource

--- editable/editable.js
+++ editable/editable.js
@@ -4,20 +4,36 @@
   function editable(options) {
     const { table, dataSource, columns } = options;
 
+    // setup thead
     let oldTableHead = table.tHead;
     if (!oldTableHead) {
       oldTableHead = table.createTHead();
     }
+    const newTableHead = document.createElement('thead');
+    const tr = document.createElement('tr');
+    columns.forEach(column => {
+      const th = document.createElement('th');
+      th.textContent = column.label;
+      tr.appendChild(th);
+    });
+    newTableHead.appendChild(tr);
+    table.replaceChild(newTableHead, oldTableHead);
 
+    // setup tbody
     let oldTableBody = table.tBodies[0];
     if (!oldTableBody) {
       oldTableBody = table.createTBody();
     }
-
-    const newTableHead = document.createElement('thead');
     const newTableBody = document.createElement('tbody');
-
-    table.replaceChild(newTableHead, oldTableHead);
+    dataSource.forEach(item => {
+      const tr = document.createElement('tr');
+      columns.forEach(column => {
+        const td = document.createElement('td');
+        td.textContent = item[column.key];
+        tr.appendChild(td);
+      });
+      newTableBody.appendChild(tr);
+    });
     table.replaceChild(newTableBody, oldTableBody);
   }
 
