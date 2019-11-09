78bd17bbf639defa0f5b4ff2d5067836875d5bf3
Hanjun Kim <hallazzang@gmail.com>
2019-11-09 23:06:44 +0900

    implement lecture search
    and add some styles

--- timetable/app.js
+++ timetable/app.js
@@ -12,12 +12,64 @@
     },
   ];
 
+  function renderLectureItem(lecture) {
+    const item = document.createElement('li');
+    item.classList.add('lectures__item');
+
+    const container = document.createElement('div');
+    container.classList.add('lectures__item-container');
+
+    const lectureName = document.createElement('span');
+    lectureName.classList.add('lectures__item-name');
+    lectureName.textContent = lecture.name;
+    container.appendChild(lectureName);
+
+    const lectureTimes = document.createElement('span');
+    lectureTimes.classList.add('lectures__item-times');
+    lectureTimes.textContent = '-';
+    container.appendChild(lectureTimes);
+
+    item.appendChild(container);
+
+    return item;
+  }
+
   function renderApp() {
     const container = document.createElement('div');
     container.classList.add('container');
 
     const lectureArea = document.createElement('div');
     lectureArea.classList.add('lecture-area');
+
+    const searchInput = document.createElement('input');
+    searchInput.classList.add('lecture-search');
+    searchInput.type = 'search';
+    searchInput.placeholder = 'Search lecture';
+    searchInput.oninput = () => {
+      updateLectureList();
+    };
+    lectureArea.appendChild(searchInput);
+
+    const lectureList = document.createElement('ul');
+    lectureList.classList.add('lectures');
+    function updateLectureList() {
+      const query = searchInput.value.trim();
+
+      while (lectureList.lastChild) {
+        lectureList.removeChild(lectureList.lastChild);
+      }
+
+      lectures
+        .filter(lecture =>
+          lecture.name.toLowerCase().includes(query.toLowerCase())
+        )
+        .forEach(lecture => {
+          lectureList.appendChild(renderLectureItem(lecture));
+        });
+    }
+    updateLectureList();
+    lectureArea.appendChild(lectureList);
+
     container.appendChild(lectureArea);
 
     const timetable = document.createElement('div');
